4e4204751168677fb96fb49fed3d5c099e95488e
Kartik <sonusaxena0013@gmail.com>
2026-08-12 10:40:53 +0530

    Fix responsive mobile layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import MainLayout from "./layouts/MainLayout";
-import "../../styles/mobile.css";
+import "./styles/mobile.css";
 
 import Home from "./pages/Home";
 import Documents from "./pages/Documents";
